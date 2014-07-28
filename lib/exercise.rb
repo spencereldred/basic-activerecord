1b0a378bf228fe11a1459e78b938c23ed967851e
Spencer Eldred <eldredspencer@gmail.com>
2014-07-28 11:18:41 -0600

    first 10 stories

--- lib/exercise.rb
+++ lib/exercise.rb
@@ -4,3 +4,45 @@ require "./lib/item"
 require "./lib/orderitem"
 require "./lib/order"
 
+customers = Customer.all  # SELECT * FROM customers;
+
+customers.each do |customer|
+  puts customer.inspect
+end
+p '*' * 20
+
+cust = Customer.first # SELECT * FROM customers Order by id limit 1;
+p cust
+p '*' * 20
+
+cust = Customer.last
+p cust
+p '*' * 20
+
+cust = Customer.find(3)
+p cust
+p '*' * 20
+
+customers = Customer.where(state: 'Colorado')
+customers.each do |customer|
+  puts customer.inspect
+end
+p '*' * 20
+
+customers = Customer.first(5)
+customers.each do |customer|
+  puts customer.inspect
+end
+p '*' * 20
+
+customers = Customer.order(:name)
+customers.each do |customer|
+  puts customer.inspect
+end
+p '*' * 20
+
+customers = Customer.order(name: :desc)
+customers.each do |customer|
+  puts customer.inspect
+end
+p '*' * 20
