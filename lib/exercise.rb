6915089737ae26a8ebf4feb704bf5fdad1ae6be7
Spencer Eldred <eldredspencer@gmail.com>
2014-07-28 12:01:55 -0600

    more stories

--- lib/exercise.rb
+++ lib/exercise.rb
@@ -7,7 +7,7 @@ require "./lib/order"
 customers = Customer.all  # SELECT * FROM customers;
 
 customers.each do |customer|
-  puts customer.inspect
+  p customer
 end
 p '*' * 20
 
@@ -25,24 +25,74 @@ p '*' * 20
 
 customers = Customer.where(state: 'Colorado')
 customers.each do |customer|
-  puts customer.inspect
+  p customer
 end
 p '*' * 20
 
 customers = Customer.first(5)
 customers.each do |customer|
-  puts customer.inspect
+  p customer
 end
 p '*' * 20
 
 customers = Customer.order(:name)
 customers.each do |customer|
-  puts customer.inspect
+
 end
 p '*' * 20
 
 customers = Customer.order(name: :desc)
 customers.each do |customer|
-  puts customer.inspect
+  p customer
 end
+p '*' * 20
+
+items = Item.all
+items.each do |item|
+  p item
+end
+p '*' * 20
+# Not Working
+# items = Item.where(Item[:name].matches('boot'))
+# items.each do |item|
+#   p item
+# end
+p '*' * 20
+
+orders = Order.all
+orders.each do |order|
+  p order
+end
+p '*' * 20
+
+orders = Order.first(5)
+orders.each do |order|
+  p order
+end
+p '*' * 20
+
+orders = Order.last(5)
+orders.each do |order|
+  p order
+end
+p '*' * 20
+
+customers = Customer.all.pluck(:name, :email)
+customers.each do |customer|
+  p customer
+end
+p '*' * 20
+
+customers = Customer.all.pluck(:name, :address)
+customers.each do |customer|
+  p customer
+end
+p '*' * 20
+
+count = Customer.count
+p count
+p '*' * 20
+
+sum = Order.sum(:amount)
+p sum.to_s.to_f
 p '*' * 20
